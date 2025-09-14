6d13459b66a6757d31f37426240a505fd1431b87
AlekBalc <balc.aleksas@gmail.com>
2025-09-14 21:12:15 +0300

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
